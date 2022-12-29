7394e80dc9718c6df7750329bd4c8a505853e72e
Segun Awe <oawe5@umd.edu>
2022-12-29 17:03:16 -0500

    Initial Commit

---
+++ Sword/SwordApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwordApp.swift
+//  Sword
+//
+//  Created by Segun Awe on 12/29/22.
+//
+
+import SwiftUI
+
+@main
+struct SwordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
