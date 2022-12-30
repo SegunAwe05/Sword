803c6c2cc86c5cbf7d215f42ac1cbd9de9e2ba7f
Segun Awe <oawe5@umd.edu>
2022-12-30 17:27:12 -0500

    finished styling and added button

--- Sword/Views/ContentView.swift
+++ Sword/Views/ContentView.swift
@@ -10,7 +10,8 @@ import SwiftUI
 struct ContentView: View {
     @State var tempList = ["Give all your worries and cares to God, for he cares about you.",
                            "Don’t worry about anything; instead, pray about everything.",
-                           "Seek the Kingdom of God above all else, and live righteously, and he will give you everything you need."]
+                           "Seek the Kingdom of God above all else, and live righteously, and he will give you everything you need.",
+                           "Keep on asking, and you will receive what you ask for. Keep on seeking, and you will find. Keep on knocking, and the door will be opened to you. 8 For everyone who asks, receives. Everyone who seeks, finds. And to everyone who knocks, the door will be opened"]
     var body: some View {
         ZStack {
             Rectangle()
@@ -35,13 +36,30 @@ struct ContentView: View {
                 Spacer()
                 List() {
                     ForEach(tempList, id: \.self) { value in
-                        CardView(verse: value)
+                        CardView(scripture: value, verse: "Mathew 6:33")
                             
                     }.listRowBackground(Color("Main-Purple"))
                         .listRowSeparator(.hidden)
                     
                 }.listStyle(PlainListStyle())
             }
+            
+            // the button
+                    VStack{
+                        Spacer()
+                        HStack {
+                            Spacer()
+                            Button {
+//                                addButton.toggle()
+                            } label: {
+                                Image(systemName: "plus")
+                                    .foregroundColor(Color("Text-Purple"))
+                                    .font(.system(size: 35))
+                            }.padding(8)
+                                .background(Color.black)
+                                .clipShape(Circle())
+                        }.padding(.trailing, 25)
+                    }.padding(.bottom, 20)
             
         }
     }
