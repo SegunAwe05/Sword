cb9ec85dc9d33ac37d44837879fb6de43667bb48
Segun Awe <oawe5@umd.edu>
2023-01-02 16:46:22 -0500

    added categories

--- Sword/Views/PostView.swift
+++ Sword/Views/PostView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct PostView: View {
     @State var scripture = ""
     @State var verse = "Matthew 6:33"
-    
+    var categories = ["Love", "Anxiety", "Healing", "Anger", "Hope", "Fear", "Peace", "Stress", "Patience", "Loss", "Joy", "Temptation", "Pride", "Doubt"]
     var body: some View {
         ZStack {
             Rectangle()
@@ -25,6 +25,7 @@ struct PostView: View {
                         .foregroundColor(Color("Text-Purple"))
                         .padding()
                     
+                    // MARK: Textfields
                     HStack {
                         Text("Scripture")
                             .foregroundColor(Color("Text-Purple"))
@@ -55,20 +56,94 @@ struct PostView: View {
                         .cornerRadius(15)
                     Spacer()
                     
-                    Button {
-                        // add tp yea
-                    } label: {
-                        Text("Add Verse")
+                    // MARK: This is where categories start
+                    Group {
+                        Text("Categories")
+                            .font(.title2)
+                            .bold()
                             .foregroundColor(Color("Text-Purple"))
-                            .frame(width: 200)
                             .padding()
-                            .background(Color("Card-Purple"))
-                            .cornerRadius(10)
+                        Text("(Select up to 3)")
+                            .foregroundColor(Color("Text-Purple"))
+                        
+                        HStack {
+                            ForEach(categories[0...3], id: \.self) { item in
+                                Button {
+                                    print(item)
+                                } label: {
+                                    Text(item)
+                                        .padding()
+                                        .background(Color("Text-Purple"))
+                                        .foregroundColor(.black)
+                                        .cornerRadius(7)
+                                }
+                            }
+                        }
+                        
+                        HStack {
+                            ForEach(categories[4...6], id: \.self) { item in
+                                Button {
+                                    print(item)
+                                } label: {
+                                    Text(item)
+                                        .padding()
+                                        .background(Color("Text-Purple"))
+                                        .foregroundColor(.black)
+                                        .cornerRadius(7)
+                                }
+                                
+                            }
+                        }
+                        
+                        HStack {
+                            ForEach(categories[7...10], id: \.self) { item in
+                                Button {
+                                    print(item)
+                                } label: {
+                                    Text(item)
+                                        .padding()
+                                        .background(Color("Text-Purple"))
+                                        .foregroundColor(.black)
+                                        .cornerRadius(7)
+                                }
+                                
+                            }
+                        }
+                        HStack {
+                            ForEach(categories[11...13], id: \.self) { item in
+                                Button {
+                                    print(item)
+                                } label: {
+                                    Text(item)
+                                        .padding()
+                                        .background(Color("Text-Purple"))
+                                        .foregroundColor(.black)
+                                        .cornerRadius(7)
+                                }
+                            }
+                        }
                     }
                     
                     Spacer()
                 }
             }
+            
+            // MARK: Add button
+            VStack{
+                Spacer()
+                HStack {
+                    Spacer()
+                    Button {
+                        //
+                    } label: {
+                        Image(systemName: "checkmark")
+                            .foregroundColor(Color("Text-Purple"))
+                            .font(.system(size: 35))
+                    }.padding(8)
+                        .background(Color.black)
+                        .clipShape(Circle())
+                }.padding(.trailing, 25)
+            }.padding(.bottom, 20)
         }
     }
 }
